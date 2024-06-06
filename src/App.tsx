57a44f414b9b948fd96785a2b3c7873b184b7c16
AnnikDev <a_kvernadze@cu.edu.ge>
2024-06-06 16:00:43 +0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,28 @@ function App() {
           <img src="/images/icon-search.svg" alt="" />
         </button>
       </Search>
+      <Main>
+        <ListenMode>
+          <div className="word">
+            <h1>word</h1>
+            <span>/"k:bc:d/</span>
+          </div>
+          <button className="iconPlay">
+            <img src="/images/icon-play.svg" alt="" />
+          </button>
+        </ListenMode>
+        <Meaning>
+          <div className="noun">
+            <h1>noun</h1>
+            <h2>Meaning</h2>
+            <h3>Synonyms</h3>
+          </div>
+          <div className="verb">
+            <h1>verb</h1>
+            <h2>Meaning</h2>
+          </div>
+        </Meaning>
+      </Main>
     </Container>
   );
 }
@@ -30,6 +52,7 @@ const Container = styled.div`
   justify-content: center;
   max-width: 100rem;
   margin: auto;
+  gap: 2rem;
 `;
 
 const Header = styled.div`
@@ -66,15 +89,37 @@ const Search = styled.div`
     color: black;
     font-weight: 600;
     padding: 1.8rem;
-    border-top-left-radius:1.5rem;
+    border-top-left-radius: 1.5rem;
     border-bottom-left-radius: 1.5rem;
   }
 
   button {
     padding-right: 1.8rem;
-    border-top-right-radius:30%;
+    border-top-right-radius: 30%;
     border-bottom-right-radius: 30%;
   }
 `;
+
+const Main = styled.div``;
+
+const ListenMode = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  h1 {
+    font-size: 7rem;
+  }
+
+  span {
+    font-size: 2rem;
+    color: #a445ed;
+  }
+
+  .iconPlay {
+    background-color: inherit;
+  }
+`;
+
+const Meaning = styled.div``;
 
 export default App;
