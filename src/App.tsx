06797e86f7da744d94d11e3f4000687366c4ac5c
AnnikDev <a_kvernadze@cu.edu.ge>
2024-06-20 12:22:06 +0400

    add Toggle Switch

--- src/App.tsx
+++ src/App.tsx
@@ -74,6 +74,10 @@ function App() {
           <span className="fonts">Serif</span>
           <img src="/images/icon-arrow-down.svg" alt="arrow" />
           <div className="line"></div>
+          <Switch>
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </Switch>
           <img src="/images/icon-moon.svg" alt="moon" />
         </div>
       </Header>
@@ -188,6 +192,70 @@ const Header = styled.div`
     height: 2.2rem;
     width: 0.1rem;
     background-color: #0000002b;
+  }
+`;
+
+const Switch = styled.label`
+  /* The switch - the box around the slider */
+
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+
+  /* Hide default HTML checkbox */
+  input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+
+  /* The slider */
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+
+  input:checked + .slider {
+    background-color: #2196f3;
+  }
+
+  input:focus + .slider {
+    box-shadow: 0 0 1px #2196f3;
+  }
+
+  input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
+
+  /* Rounded sliders */
+  .slider.round {
+    border-radius: 34px;
+  }
+
+  .slider.round:before {
+    border-radius: 50%;
   }
 `;
 
